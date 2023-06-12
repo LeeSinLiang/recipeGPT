8fed37fcadb14538244d03ba46bf870f8f32bd46
Architect <mangojamlee@gmail.com>
2023-06-12 20:26:43 +0800

    'bpe'

--- app.py
+++ app.py
@@ -45,13 +45,13 @@ st.title('microGPT: Generate Recipe! 😋')
 
 # print(decode(encode('Hello World')))
 with open('config/config.json', 'r') as f:
-    config = json.load(f)
+	config = json.load(f)
 
 cfg = GPTConfig(**config)
 
 if os.path.exists('models/recipeGPT-v1.pth') is False:
-    with st.spinner('Downloading Model... (This might take a while)'):
-    	download_with_progress('https://github.com/LeeSinLiang/recipeGPT/releases/download/v1/recipeGPT-v1.pth', 'models/recipeGPT-v1.pth')
+	with st.spinner('Downloading Model... (This might take a while)'):
+		download_with_progress('https://github.com/LeeSinLiang/recipeGPT/releases/download/v1/recipeGPT-v1.pth', 'models/recipeGPT-v1.pth')
 
 with st.spinner('Loading Model...'):
 	model = load_model('models/recipeGPT-v1.pth', cfg)
@@ -71,21 +71,24 @@ with st.form("input"):
 	submitted = st.form_submit_button("Submit")
 
 if submitted:
-	with torch.no_grad():
-		# print(tokenizer.decode(model.generate(context, max_tokens_generate=500).tolist()))
-		context_str = 'Title: ' + context + '\n'
-		context = torch.tensor(tokenizer.encode(context_str), dtype=torch.long, device=device).reshape(1, -1)
-		progress_text = "Operation in progress. Please wait."
-		progress_bar = st.progress(0, text=progress_text)
-		placeholder = st.empty()
-		output_fn = getattr(placeholder, 'text')
-		with StringIO() as buffer:
-			def out_fn(b):
-				buffer.write(b + '')
-				output_fn(buffer.getvalue() + '')
-			out_fn(context_str)
-			response = tokenizer.decode(model.generate(tokenizer, out_fn=out_fn, progress=(progress_bar, progress_text), idx=context, max_tokens_generate=n_tokens, temperature=temperature, top_k=top_k, top_p=top_p).tolist())
-		st.balloons()
+	if context is None or context == '':
+		st.error('Title cannot be empty.', icon="🚨")
+	else:
+		with torch.no_grad():
+			# print(tokenizer.decode(model.generate(context, max_tokens_generate=500).tolist()))
+			context_str = 'Title: ' + context + '\n'
+			context = torch.tensor(tokenizer.encode(context_str), dtype=torch.long, device=device).reshape(1, -1)
+			progress_text = "Operation in progress. Please wait."
+			progress_bar = st.progress(0, text=progress_text)
+			placeholder = st.empty()
+			output_fn = getattr(placeholder, 'text')
+			with StringIO() as buffer:
+				def out_fn(b):
+					buffer.write(b + '')
+					output_fn(buffer.getvalue() + '')
+				out_fn(context_str)
+				response = tokenizer.decode(model.generate(tokenizer, out_fn=out_fn, progress=(progress_bar, progress_text), idx=context, max_tokens_generate=n_tokens, temperature=temperature, top_k=top_k, top_p=top_p).tolist())
+			st.balloons()
 
 footer="""<style>
 a:link , a:visited{
